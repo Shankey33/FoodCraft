0b40f49104159b9012ad369752e546e7214fa073
Shankey33 <aadidwivedigwl@gmail.com>
2025-08-12 12:30:37 +0530

    removed home and sign till further notice

--- src/main.jsx
+++ src/main.jsx
@@ -8,22 +8,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import logoFavicon from './assets/logo.png';
 
-// Dynamically set (or replace) the favicon to avoid 404s when deploying under a non-root base path (e.g. GitHub Pages)
-// const ensureFavicon = (href) => {
-//   if (typeof document === 'undefined') return;
-//   let link = document.querySelector("link[rel~='icon']");
-//   if (!link) {
-//     link = document.createElement('link');
-//     link.rel = 'icon';
-//     document.head.appendChild(link);
-//   }
-//   if (link.href !== href) {
-//     link.href = href;
-//   }
-// };
-// ensureFavicon(logoFavicon);
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
